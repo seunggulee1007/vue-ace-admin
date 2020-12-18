86216026426b9a36eae084e90058ccbc9894af29
leeseunggu <leesg107@naver.com>
2020-12-19 05:36:34 +0900

    vuex 모듈 단위로 구분
    기존에 한 파일로 통합되어 있던 것을 기능에 따라 모듈별로 구분

--- src/store/store.js
+++ src/store/store.js
@@ -1,18 +1,10 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
-import actions from './actions';
-import mutations from './mutations';
-import getters from './getters';
-import { getCookie } from '../utils/cookies';
+import Auth from './auth/auth';
 
 Vue.use(Vuex);
 export default new Vuex.Store({
-	state: {
-		userId: getCookie(process.env.VUE_APP_USER_ID) || '',
-		userNm: getCookie(process.env.VUE_APP_USER_NM) || '',
-		token: getCookie(process.env.VUE_APP_AUTH_TOKEN) || '',
+	modules: {
+		Auth,
 	},
-	getters,
-	mutations,
-	actions,
 });
